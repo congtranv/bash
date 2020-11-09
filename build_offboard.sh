a500ff9be2c101c8979eb4a75ef7f978ac2c2125
congtranv <56678165+congtranv@users.noreply.github.com>
2020-11-09 18:12:48 +0700

    Update build_offboard.sh

--- build_offboard.sh
+++ build_offboard.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clear
-cd ~/ros/catkin_ws
+cd ~/ros/catkin_ws #need changing to your path if has deference
 catkin build offboard
 wait
-cd ~/ros/bash
+cd ~/ros/bash #need changing to your path if has deference
