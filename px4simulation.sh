ea693ceff45feb3356f718f01679246ada3ac35b
congtranv <56678165+congtranv@users.noreply.github.com>
2020-11-09 19:07:07 +0700

    Update px4simulation.sh

--- px4simulation.sh
+++ px4simulation.sh
@@ -4,12 +4,12 @@ source ~/.bashrc
 wait
 clear
 
-echo "Export HOME gps coordinates"
+#echo "Export HOME gps coordinates"
 #export PX4_HOME_LAT=21.0065641
 #export PX4_HOME_LON=105.8429146
 #export PX4_HOME_ALT=0
-echo "HOME [$PX4_HOME_LAT, $PX4_HOME_LON, $PX4_HOME_ALT]"
-wait
+#echo "HOME [$PX4_HOME_LAT, $PX4_HOME_LON, $PX4_HOME_ALT]"
+#wait
 
 echo "run simulation"
 roslaunch px4 mavros_posix_sitl.launch
